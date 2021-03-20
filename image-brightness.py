482127a1d574259eedfff6d25a7d657127d3f46c
Gustavo Y. Maruyama <gustavo.maruyama@ifms.edu.br>
2021-03-20 19:07:32 -0400

    refactor

--- image-brightness.py
+++ image-brightness.py
@@ -7,11 +7,11 @@ w, h, nc = img.shape
 
 new_image = np.zeros((w,h), dtype= np.uint8)
 
-for w in range(w):
+for x in range(w):
     for y in range(h):
-        red_c = int(img[w,y,0])
-        green_c = int(img[w,y,2])
-        blue_c = int(img[w,y,1])
+        red_c = int(img[x,y,0])
+        green_c = int(img[x,y,2])
+        blue_c = int(img[x,y,1])
         new_image[w,y] = (max(red_c, blue_c, green_c) + min (red_c, blue_c, green_c))/2
 
 io.imsave('lenna-brightness.jpg', new_image)
